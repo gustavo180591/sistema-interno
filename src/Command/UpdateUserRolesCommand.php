0648c55f612ac5947da48a637affb0de5a11ea86
matudelatower <matias.delatorre89@gmail.com>
2025-09-09 21:32:43 -0300

    fix .gitingore fix UpdateUserRolesCommand

--- src/Command/UpdateUserRolesCommand.php
+++ src/Command/UpdateUserRolesCommand.php
@@ -4,17 +4,19 @@ namespace App\Command;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+#[AsCommand(
+    name: 'app:user:update-roles',
+    description: 'Update user roles'
+)]
 class UpdateUserRolesCommand extends Command
 {
-    protected static $defaultName = 'app:user:update-roles';
-    protected static $defaultDescription = 'Update user roles';
-
     private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager)
